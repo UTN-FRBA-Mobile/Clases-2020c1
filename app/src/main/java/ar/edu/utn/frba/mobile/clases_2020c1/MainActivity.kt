22ecd8b94234519cee923a8d424ca4c23db688bf
Juan Brito <juan.a.brito90@gmail.com>
2020-04-06 00:57:08 -0300

    Clase 02 - Arquitectura base: - Activity - Fragment - Comunicacion entre Activity y Fragment

--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c1/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c1/MainActivity.kt
@@ -2,17 +2,24 @@ package ar.edu.utn.frba.mobile.clases_2020c1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import ar.edu.utn.frba.mobile.clases_2020c1.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
+                    .replace(R.id.container, MainFragment.newInstance(getString(R.string.soy_un_textview)))
                     .commitNow()
         }
+    }
+
+    override fun onButtonTapped() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.hello)
+            .setPositiveButton(R.string.ok) { _, _ -> }
+            .create().show()
     }
 }
--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c1/ui/main/MainFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c1/ui/main/MainFragment.kt
@@ -1,24 +1,51 @@
 package ar.edu.utn.frba.mobile.clases_2020c1.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import ar.edu.utn.frba.mobile.clases_2020c1.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
+private const val MAIN_FRAGMENT_ARG_LABEL = "MAIN_FRAGMENT_ARG_LABEL"
 
 class MainFragment : Fragment() {
+    private var label: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+    private lateinit var viewModel: MainViewModel
 
     companion object {
-        fun newInstance() = MainFragment()
+        @JvmStatic
+        fun newInstance(title: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(MAIN_FRAGMENT_ARG_LABEL, title)
+                }
+            }
     }
 
-    private lateinit var viewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            label = it.getString(MAIN_FRAGMENT_ARG_LABEL)
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textView).text = label
+        button.setOnClickListener {
+            onButtonPressed()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -27,4 +54,25 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    private fun onButtonPressed() {
+        listener?.onButtonTapped()
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onButtonTapped()
+    }
 }
